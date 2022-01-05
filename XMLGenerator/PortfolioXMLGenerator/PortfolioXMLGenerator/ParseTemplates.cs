abdb0f44b3329062b4b266b8fc25565579f046a0
Coyne, Sophie <rcoyneyr82011@gmail.com>
2022-01-05 12:54:01 +0000

    Added reflection parser and parse templates

---
+++ XMLGenerator/PortfolioXMLGenerator/PortfolioXMLGenerator/ParseTemplates.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioXMLGenerator
+{
+    public enum PROTECTION
+    {
+        PRIVATE,
+        PROTECTED,
+        PUBLIC
+    }
+    public struct ParsedAssembly
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        Dictionary<string, ParsedType> parsedTypes;
+        public ParsedType[] ParsedTypes
+        {
+            get
+            {
+                return parsedTypes.Values.ToArray();
+            }
+        }
+
+        public ParsedAssembly(string _name)
+        {
+            name = _name;
+            parsedTypes = new Dictionary<string, ParsedType>();
+        }
+
+        public void AddType(ParsedType type)
+        {
+            parsedTypes.Add(type.Name, type);
+        }
+    }
+
+    public struct ParsedType
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string Description { get; set; }
+
+        List<ParsedMethod> methods;
+        //Dictionary<string, ParsedMethod> methods;
+        public ParsedMethod[] Methods
+        {
+            get
+            {
+                return methods.ToArray();
+                //return methods.Values.ToArray();
+            }
+        }
+
+        List<ParsedVariable> variables;
+        public ParsedVariable[] Variables
+        {
+            get
+            {
+                return variables.ToArray();
+            }
+        }
+
+        List<ParsedProperty> properties;
+        public ParsedProperty[] Properties
+        {
+            get
+            {
+                return properties.ToArray();
+            }
+        }
+
+        public ParsedType(string _name)
+        {
+            name = _name;
+            Description = "";
+            methods = new List<ParsedMethod>();
+            variables = new List<ParsedVariable>();
+            properties = new List<ParsedProperty>();
+        }
+
+        public void AddMethod(ParsedMethod method)
+        {
+            methods.Add(method);
+            //methods.Add(method.Name, method);
+        }
+
+        public void AddVariable(ParsedVariable variable)
+        {
+            variables.Add(variable);
+        }
+
+        public void AddProperty(ParsedProperty property)
+        {
+            properties.Add(property);
+        }
+    }
+
+    public struct ParsedVariable
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        string type;
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+
+        public ParsedVariable(string _name, string _type)
+        {
+            name = _name;
+            type = _type;
+        }
+    }
+
+    public struct ParsedMethod
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        string returnType;
+        public string ReturnType
+        {
+            get
+            {
+                return returnType;
+            }
+        }
+
+        PROTECTION protectionLevel;
+        public PROTECTION ProtectionLevel
+        {
+            get
+            {
+                return protectionLevel;
+            }
+        }
+
+        List<ParsedParameter> parameters;
+
+        public ParsedMethod(string _name, string _returnType, PROTECTION protection)
+        {
+            name = _name;
+            returnType = _returnType;
+            parameters = new List<ParsedParameter>();
+            protectionLevel = protection;
+        }
+
+        public void AddParameter(ParsedParameter parameter)
+        {
+            parameters.Add(parameter);
+        }
+    }
+
+    public struct ParsedParameter
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+    public struct ParsedProperty
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        List<ParsedAccessor> accessors;
+        public ParsedAccessor[] Accessors
+        {
+            get
+            {
+                return accessors.ToArray();
+            }
+        }
+
+        public ParsedProperty(string _name)
+        {
+            name = _name;
+            accessors = new List<ParsedAccessor>();
+        }
+
+        public void AddAccessor(ParsedAccessor accessor)
+        {
+            accessors.Add(accessor);
+        }
+    }
+
+    public enum ACCESSOR_TYPE
+    {
+        GETTER,
+        SETTER
+    }
+
+    public struct ParsedAccessor
+    {
+        ACCESSOR_TYPE type;
+        public ACCESSOR_TYPE Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+
+        PROTECTION protectionLevel;
+        public PROTECTION ProtectionLevel
+        {
+            get
+            {
+                return protectionLevel;
+            }
+        }
+
+        public ParsedAccessor(ACCESSOR_TYPE _type, PROTECTION _protectionLevel)
+        {
+            type = _type;
+            protectionLevel = _protectionLevel;
+        }
+    }
+}
---
+++ XMLGenerator/PortfolioXMLGenerator/PortfolioXMLGenerator/ReflectionParser.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioXMLGenerator
+{
+    /// <summary>
+    /// A utility for "parsing" assemblies.
+    /// </summary>
+    public static class ReflectionParser
+    {
+        public static bool ParseAssembly(string file, out ParsedAssembly parsedAssembly)
+        {
+            parsedAssembly = new ParsedAssembly();
+            if (!File.Exists(file))
+            {
+                return false;
+            }
+            
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(file);
+
+                parsedAssembly = new ParsedAssembly(assembly.FullName);
+
+                Type[] types = assembly.GetTypes();
+
+                foreach(Type type in types)
+                {
+                    if (!type.IsEnum)
+                    {
+                        ParsedType parsedType = new ParsedType(type.Name);
+
+                        foreach (FieldInfo field in type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
+                        {
+                            ParsedVariable parsedVariable = new ParsedVariable(field.Name, field.FieldType.ToString());
+
+                            parsedType.AddVariable(parsedVariable);
+                        }
+
+                        foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+                        {
+                            
+                            ParsedMethod parsedMethod = new ParsedMethod(method.Name, method.ReturnType.ToString(), method.GetProtectionLevel());
+
+                            ParameterInfo[] parameters = method.GetParameters();
+
+
+
+                            foreach (ParameterInfo parameter in parameters)
+                            {
+                                ParsedParameter parsedParameter = new ParsedParameter();
+                                parsedParameter.Name = parameter.Name;
+                                parsedParameter.Type = parameter.ParameterType.ToString();
+                                parsedMethod.AddParameter(parsedParameter);
+                            }
+
+                            parsedType.AddMethod(parsedMethod);
+                        }
+
+                        foreach(PropertyInfo property in type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+                        {
+                            ParsedProperty parsedProperty = new ParsedProperty(property.Name);
+
+                            MethodInfo getter = property.GetGetMethod(true);
+
+                            if (getter != null)
+                            {
+                                ParsedAccessor accessor = new ParsedAccessor(ACCESSOR_TYPE.GETTER, getter.GetProtectionLevel());
+                                parsedProperty.AddAccessor(accessor);
+                            }
+
+                            MethodInfo setter = property.GetSetMethod(true);
+
+                            if (setter != null)
+                            {
+                                ParsedAccessor accessor = new ParsedAccessor(ACCESSOR_TYPE.SETTER, setter.GetProtectionLevel());
+                                parsedProperty.AddAccessor(accessor);
+                            }
+
+                            parsedType.AddProperty(parsedProperty);
+                        }
+
+                        parsedAssembly.AddType(parsedType);
+                    }
+                }
+
+                return true;
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine("ERROR: "+err.Message);
+                return false;
+            }
+        }
+
+        public static int P
+        {
+            get
+            {
+                return 4;
+            }
+        }
+
+        public static PROTECTION GetProtectionLevel(this MethodInfo method)
+        {
+            PROTECTION protection = method.IsPublic ? PROTECTION.PUBLIC :
+            (
+                method.IsPrivate ? PROTECTION.PRIVATE : PROTECTION.PROTECTED
+            );
+            return protection;
+        }
+
+        //public static PROTECTION GetProtectionLevel(this MemberInfo memberInfo)
+        //{
+            
+        //}
+    }
+}
