ba5279ce97b0169ee389c81609c46c2b88f69ec8
Coyne, Sophie <rcoyneyr82011@gmail.com>
2022-01-11 19:47:08 +0000

    Created utility for XML functionality

---
+++ XMLGenerator/PortfolioXMLGenerator/PortfolioXMLGenerator/XML_Util.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace PortfolioXMLGenerator
+{
+    public static class XML_Util
+    {
+        static Dictionary<string, PROTECTION> protectionLevels = new Dictionary<string, PROTECTION>()
+        {
+            {"private",PROTECTION.PRIVATE},
+            {"protected",PROTECTION.PROTECTED},
+            {"internal",PROTECTION.INTERNAL},
+            {"public",PROTECTION.PUBLIC}
+        };
+
+        public static bool StringContainsProtection(this string str, out PROTECTION protection)
+        {
+            string key = str.ToLower();
+            if (protectionLevels.ContainsKey(key))
+            {
+                protection = protectionLevels[key];
+                return true;
+            }
+            protection = default(PROTECTION);
+            return false;
+        }
+
+        public static bool IsStartElement(this XmlReader xmlReader, PORTFOLIO_XML_ELEMENT element)
+        {
+            return xmlReader.IsStartElement(element.ToString().ToLower());
+        }
+
+        public static string GetAttribute(this XmlReader xmlReader, PORTFOLIO_XML_ATTRIBUTE attr)
+        {
+            return xmlReader[attr.ToString().ToLower()];
+        }
+
+        public static void WriteStartElementEnum(this XmlWriter writer, PORTFOLIO_XML_ELEMENT element)
+        {
+            writer.WriteStartElement(element.ToString().ToLower());
+        }
+
+        public static void WriteAttributeEnum(this XmlWriter writer, PORTFOLIO_XML_ATTRIBUTE attribute, object obj)
+        {
+            writer.WriteAttributeString(attribute.ToString().ToLower(), obj.ToString());
+        }
+    }
+    public enum PORTFOLIO_XML_ELEMENT
+    {
+        root,
+        name,
+        description,
+        variables,
+        variable,
+        methods,
+        method,
+        parameter,
+        properties,
+        property,
+        classconst,
+        NAMESPACE
+    }
+    public enum PORTFOLIO_XML_ATTRIBUTE
+    {
+        name,
+        type,
+        protection,
+        description,
+        getter,
+        setter,
+        is_static,
+        getterStatic,
+        setterStatic,
+        NAMESPACE
+    }
+}
