3ade7d97bbd2c49c7dfd4d0572e9699c9e27c8ce
Coyne, Sophie <rcoyneyr82011@gmail.com>
2022-01-21 18:38:15 +0000

    Created utility for logging directly into copyable and rewritable format

---
+++ XMLGenerator/PortfolioXMLGenerator/PortfolioXMLGenerator/LogAlert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortfolioXMLGenerator
+{
+    public partial class LogAlert : Form
+    {
+        private LogAlert(string text)
+        {
+            InitializeComponent();
+            rtb.Text = text;
+        }
+        public static void Show(string text)
+        {
+            new LogAlert(text).Show();
+        }
+    }
+}
