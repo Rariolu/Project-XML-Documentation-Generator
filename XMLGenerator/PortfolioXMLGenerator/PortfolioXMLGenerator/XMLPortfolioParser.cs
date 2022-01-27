91626e2a5922d90bc820253db32c2cba18d30a9f
Coyne, Sophie <rcoyneyr82011@gmail.com>
2022-01-27 18:39:33 +0000

    Implemented variable, property, and method parsing.

--- XMLGenerator/PortfolioXMLGenerator/PortfolioXMLGenerator/XMLPortfolioParser.cs
+++ XMLGenerator/PortfolioXMLGenerator/PortfolioXMLGenerator/XMLPortfolioParser.cs
@@ -27,7 +27,7 @@ namespace PortfolioXMLGenerator
                 ParsedType parsedType;
                 if (ParseType(xmlFile, out parsedType))
                 {
-                    //assembly.AddType(parsedType);
+                    assembly.AddType(parsedType);
                 }
             }
 
@@ -36,7 +36,6 @@ namespace PortfolioXMLGenerator
 
         public static bool ParseType(string file, out ParsedType parsedType)
         {
-            throw new NotImplementedException();
             parsedType = null;
             if (!File.Exists(file))
             {
@@ -73,7 +72,7 @@ namespace PortfolioXMLGenerator
 
                 if (xmlReader.IsStartElement(PORTFOLIO_XML_ELEMENT.variables))
                 {
-                    xmlReader.Read();
+                    //xmlReader.Read();
 
                     XmlReader variables = xmlReader.ReadSubtree();
                     while(variables.Read())
@@ -84,13 +83,131 @@ namespace PortfolioXMLGenerator
                             string varName = variables.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.name);
                             string varNamespace = variables.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.NAMESPACE);
                             string isStatic = variables.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.is_static);
+                            string protStr = variables.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.protection);
                             PROTECTION protection;
-                            if (variables.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.protection).StringContainsProtection(out protection))
+                            bool isStaticBool;
+                            if (!protStr.StringContainsProtection(out protection))
                             {
-                                //ParsedVariable parsedVariable = new ParsedVariable(varName, type, varNamespace, protection, isStatic);
+                                protection = PROTECTION.PRIVATE;
+                            }
+                            if (!isStatic.StringContainsBool(out isStaticBool))
+                            {
+                                isStaticBool = false;
                             }
 
+                            ParsedVariable parsedVariable = new ParsedVariable(varName, type, varNamespace, protection, isStaticBool);
 
+                            string description = variables.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.description);
+
+                            if (!string.IsNullOrEmpty(description))
+                            {
+                                parsedVariable.Description = description;
+                            }
+                            
+                            parsedType.AddVariable(parsedVariable);
+                        }
+                    }
+                }
+                else if (xmlReader.IsStartElement(PORTFOLIO_XML_ELEMENT.methods))
+                {
+                    //xmlReader.Read();
+
+                    XmlReader methods = xmlReader.ReadSubtree();
+                    while(methods.Read())
+                    {
+                        if (methods.IsStartElement(PORTFOLIO_XML_ELEMENT.classconst))
+                        {
+                            string protStr = methods.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.protection);
+                            PROTECTION protection;
+
+                            if (!protStr.StringContainsProtection(out protection))
+                            {
+                                protection = PROTECTION.PRIVATE;
+                            }
+
+                            ParsedConstructor parsedConstructor = new ParsedConstructor(protection);
+
+                            //methods.Read();
+
+                            XmlReader parameters = methods.ReadSubtree();
+
+                            ParseParameters(parameters, parsedConstructor);
+
+                            parsedType.AddMethod(parsedConstructor, true);
+                        }
+                        else if (methods.IsStartElement(PORTFOLIO_XML_ELEMENT.method))
+                        {
+                            string type = methods.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.type);
+                            string methodName = methods.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.name);
+                            string methodNamespace = methods.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.NAMESPACE);
+                            string protStr = methods.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.protection);
+                            string staticStr = methods.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.is_static);
+
+                            PROTECTION protection;
+                            bool isStatic;
+                            if (!protStr.StringContainsProtection(out protection))
+                            {
+                                protection = PROTECTION.PRIVATE;
+                            }
+                            if (!staticStr.StringContainsBool(out isStatic))
+                            {
+                                isStatic = false;
+                            }
+
+                            ParsedMethod parsedMethod = new ParsedMethod(methodName, type, methodNamespace, protection, isStatic);
+
+                            string description = methods.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.description);
+                            parsedMethod.Description = description;
+
+                            //methods.Read();
+
+                            XmlReader parameters = methods.ReadSubtree();
+
+                            ParseParameters(parameters, parsedMethod);
+
+                            parsedType.AddMethod(parsedMethod);
+                        }
+                    }
+                }
+                else if (xmlReader.IsStartElement(PORTFOLIO_XML_ELEMENT.properties))
+                {
+                    //xmlReader.Read();
+
+                    XmlReader properties = xmlReader.ReadSubtree();
+                    
+                    while(properties.Read())
+                    {
+                        if (properties.IsStartElement(PORTFOLIO_XML_ELEMENT.property))
+                        {
+                            string type = properties.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.type);
+                            string propertyName = properties.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.name);
+                            string propertyNamespace = properties.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.NAMESPACE);
+
+                            string getterStr = properties.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.getter);
+                            string getterStaticStr = properties.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.getterStatic);
+                            string setterStr = properties.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.setter);
+                            string setterStaticStr = properties.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.setterStatic);
+
+                            ParsedProperty parsedProperty = new ParsedProperty(propertyName, type, propertyNamespace);
+                            PROTECTION getter;
+                            if (getterStr.StringContainsProtection(out getter))//StringContainsEnum(out getter))
+                            {
+                                bool getterStatic;
+                                getterStaticStr.StringContainsBool(out getterStatic);
+
+                                parsedProperty.AddAccessor(ACCESSOR_TYPE.GETTER, getter, getterStatic);
+                            }
+
+                            PROTECTION setter;
+                            if (setterStr.StringContainsProtection(out setter))
+                            {
+                                bool setterStatic;
+                                setterStaticStr.StringContainsBool(out setterStatic);
+
+                                parsedProperty.AddAccessor(ACCESSOR_TYPE.SETTER, setter, setterStatic);
+                            }
+
+                            parsedType.AddProperty(parsedProperty);
                         }
                     }
                 }
@@ -98,6 +215,25 @@ namespace PortfolioXMLGenerator
 
             xmlReader.Close();
             return true;
+        }
+
+        static void ParseParameters(XmlReader parameters, ParsedMethod parsedMethod)
+        {
+            while (parameters.Read())
+            {
+                if (parameters.IsStartElement(PORTFOLIO_XML_ELEMENT.parameter))
+                {
+                    string paramType = parameters.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.type);
+                    string paramName = parameters.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.name);
+                    string paramNamespace = parameters.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.NAMESPACE);
+                    string paramDescription = parameters.GetAttribute(PORTFOLIO_XML_ATTRIBUTE.description);
+
+                    ParsedParameter parsedParameter = new ParsedParameter(paramName, paramType, paramNamespace);
+                    parsedParameter.Description = paramDescription;
+
+                    parsedMethod.AddParameter(parsedParameter);
+                }
+            }
         }
     }
 }
