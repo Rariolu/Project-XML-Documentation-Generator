027a89b9c927797419d550ca203c3247268850bc
Coyne, Sophie <rcoyneyr82011@gmail.com>
2022-01-27 18:38:44 +0000

    Created "NormaliseString" function to normalise strings and "StringContainsEnum" function to get enum values from strings.

--- XMLGenerator/PortfolioXMLGenerator/PortfolioXMLGenerator/Util.cs
+++ XMLGenerator/PortfolioXMLGenerator/PortfolioXMLGenerator/Util.cs
@@ -9,6 +9,11 @@ namespace PortfolioXMLGenerator
 {
     public static class Util
     {
+        public static string NormaliseString(this object obj)
+        {
+            return obj.ToString().ToLower().Trim();
+        }
+
         /// <summary>
         /// Remove invalid characters from a path string.
         /// Taken almost verbatim from a StackOverflow answer:
@@ -25,15 +30,50 @@ namespace PortfolioXMLGenerator
             return string.Empty;
         }
 
+        /// <summary>
+        /// Check a string to check if it contains a boolean value (i.e. "true" or "false") and sets the "val" parameter to the appropriate value if that is the case.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
         public static bool StringContainsBool(this string str, out bool val)
         {
-            str = str.ToLower();
+            str = str.NormaliseString();
             if (str == "true" || str == "false")
             {
                 val = str == "true";
                 return true;
             }
             val = false;
+            return false;
+        }
+
+        /// <summary>
+        /// Checks a given string to determine if it contains an enum value of a given type.
+        /// </summary>
+        /// <typeparam name="T">Desired enum type</typeparam>
+        /// <param name="str">Text that is checked.</param>
+        /// <param name="val">The outputted enum value.</param>
+        /// <returns></returns>
+        public static bool StringContainsEnum<T>(this string str, out T val) where T : struct
+        {
+            Type type = typeof(T);
+            val = default(T);
+            if (!type.IsEnum)
+            {
+                return false;
+            }
+
+            Array enumValues = Enum.GetValues(type);
+            foreach(T enumVal in enumValues)
+            {
+                if (str.NormaliseString() == enumVal.NormaliseString())
+                {
+                    val = enumVal;
+                    return true;
+                }
+            }
+
             return false;
         }
     }
