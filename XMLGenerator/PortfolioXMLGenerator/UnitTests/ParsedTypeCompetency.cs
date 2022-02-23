3ad732bd8cbe6ed30ffaf37e8dfb7610aa11bc9d
Rariolu <rcoyneyr82011@gmail.com>
2022-02-23 00:57:55 +0000

    Created unit tests project and created comparison tests

---
+++ XMLGenerator/PortfolioXMLGenerator/UnitTests/ParsedTypeCompetency.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PortfolioGeneratorBackend;
+
+namespace UnitTests
+{
+    public static class ParsedTypeCompetency
+    {
+        public static void TrueComparisons()
+        {
+            ParsedVariable pv1 = new ParsedVariable("pv", "String", "System", PROTECTION.PRIVATE);
+            ParsedVariable pv2 = new ParsedVariable("pv", "String", "System", PROTECTION.PRIVATE);
+
+            if (pv1 != pv2)
+            {
+                throw new Exception("ParsedVariable comparison failed.");
+            }
+
+            ParsedMethod pm1 = new ParsedMethod("pm", "Int32", "System", PROTECTION.PROTECTED);
+            ParsedMethod pm2 = new ParsedMethod("pm", "Int32", "System", PROTECTION.PROTECTED);
+
+            if (pm1 != pm2)
+            {
+                throw new Exception("ParsedMethod comparison failed.");
+            }
+        }
+        public static void FalseComparisons()
+        {
+            ParsedVariable pv1 = new ParsedVariable("pv", "String", "System", PROTECTION.PRIVATE);
+            ParsedVariable pv2 = new ParsedVariable("pv", "String", "System", PROTECTION.INTERNAL);
+
+            if (pv1 == pv2)
+            {
+                throw new Exception("ParsedVariable comparison failed.");
+            }
+        }
+    }
+}
---
+++ XMLGenerator/PortfolioXMLGenerator/UnitTests/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformCheck(ParsedTypeCompetency.TrueComparisons, "TrueComparisons");
+            PerformCheck(ParsedTypeCompetency.FalseComparisons, "FalseComparisons");
+            Console.ReadKey();
+        }
+
+        static void PerformCheck(Action method, string name)
+        {
+            try
+            {
+                method();
+                Console.WriteLine("{0} succeeded.", name);
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine("{0} failed with message \"{1}\"", name, err.Message);
+            }
+        }
+    }
+}
