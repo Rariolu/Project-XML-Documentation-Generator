899dae0c56f614beb2222b6a63416f96a84fb78f
Rariolu <rcoyneyr82011@gmail.com>
2022-02-24 23:02:58 +0000

    Added variables of varying protection levels and staticness.

--- XMLGenerator/PortfolioXMLGenerator/DemoAssembly/Classes.cs
+++ XMLGenerator/PortfolioXMLGenerator/DemoAssembly/Classes.cs
@@ -8,6 +8,15 @@ namespace DemoAssembly
 {
     public class ClassA
     {
+        private int priv = 4;
+        public bool pub = true;
+        private protected int privProt = 42;
+        protected internal string protIntern = "24";
+
+        static int privStatic = 4;
+        public static bool pubStatic = true;
+        private protected static int privProtStatic = 42;
+        protected internal static string protInternStatic = "24";
     }
 
     public class ClassB
