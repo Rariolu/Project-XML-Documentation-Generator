fe2705f6d5e3d899d91387aa647038d97bef3977
Rariolu <rcoyneyr82011@gmail.com>
2022-02-25 01:43:02 +0000

    Added an enum type

---
+++ XMLGenerator/PortfolioXMLGenerator/DemoAssembly/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAssembly
+{
+    public enum BLOOP
+    {
+        TWO = 2,
+        THREE = 3
+    }
+}
