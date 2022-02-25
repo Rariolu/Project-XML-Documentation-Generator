83f828fb0ac8d69230b112d262f0ac3f7d1cdd69
Rariolu <rcoyneyr82011@gmail.com>
2022-02-25 00:20:32 +0000

    Added more comparisons

--- XMLGenerator/PortfolioXMLGenerator/UnitTests/ParsedTypeCompetency.cs
+++ XMLGenerator/PortfolioXMLGenerator/UnitTests/ParsedTypeCompetency.cs
@@ -26,6 +26,22 @@ namespace UnitTests
             {
                 throw new Exception("ParsedMethod comparison failed.");
             }
+
+            ParsedConstructor pc1 = new ParsedConstructor(PROTECTION.PUBLIC);
+            ParsedConstructor pc2 = new ParsedConstructor(PROTECTION.PUBLIC);
+
+            if (pc1 != pc2)
+            {
+                throw new Exception("ParsedConstructor comparison failed.");
+            }
+
+            ParsedProperty pp1 = new ParsedProperty("urgh", "Misc", "");
+            ParsedProperty pp2 = new ParsedProperty("urgh", "Misc", "");
+
+            if (pp1 != pp2)
+            {
+                throw new Exception("ParsedProperty comparison failed.");
+            }
         }
         public static void FalseComparisons()
         {
@@ -35,6 +51,30 @@ namespace UnitTests
             if (pv1 == pv2)
             {
                 throw new Exception("ParsedVariable comparison failed.");
+            }
+
+            ParsedMethod pm1 = new ParsedMethod("pm", "Int32", "System", PROTECTION.PROTECTED);
+            ParsedMethod pm2 = new ParsedMethod("pm", "Int32", "System", PROTECTION.PUBLIC);
+
+            if (pm1 == pm2)
+            {
+                throw new Exception("ParsedMethod comparison failed.");
+            }
+
+            ParsedConstructor pc1 = new ParsedConstructor(PROTECTION.PUBLIC);
+            ParsedConstructor pc2 = new ParsedConstructor(PROTECTION.PRIVATE_PROTECTED);
+
+            if (pc1 == pc2)
+            {
+                throw new Exception("ParsedConstructor comparison failed.");
+            }
+
+            ParsedProperty pp1 = new ParsedProperty("urgh", "Misc", "");
+            ParsedProperty pp2 = new ParsedProperty("urgh", "Misc", "System");
+
+            if (pp1 == pp2)
+            {
+                throw new Exception("ParsedProperty comparison failed.");
             }
         }
     }
